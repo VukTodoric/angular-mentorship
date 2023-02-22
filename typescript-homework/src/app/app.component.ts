e30ee083cb749f7054f8f60c7d38ae195c9ba76f
Vuk Todoric <vuk.todoric@ey.com>
2023-02-22 12:18:32 +0100

    Candidate class

--- typescript-homework/src/app/app.component.ts
+++ typescript-homework/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Candidate } from './classes/candidate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'typescript-homework';
+
+  candidate1: Candidate = new Candidate(
+    'Petar',
+    'Petrovic',
+    new Date('1991-01-01')
+  );
+  candidate2: Candidate = new Candidate(
+    'Marko',
+    'Markovic',
+    new Date('1992-02-02')
+  );
+
+  ngOnInit(): void {
+    console.log('Canditate 1 is ->', this.candidate1);
+    console.log('Canditate 2 is ->', this.candidate2);
+  }
 }
---
+++ typescript-homework/src/app/classes/candidate.ts
@@ -0,0 +1,7 @@
+export class Candidate {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    private birthday: Date
+  ) {}
+}
